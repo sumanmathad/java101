3f5c9251f1d0db7b6126e2fcfdd6810e9eee8649
suman.mathad <suman.mathad@oracle.com>
2024-10-26 19:04:05 +0530

    added a file for Math class functions

---
+++ src/test/java/coreAPIs/MathFunctions.java
@@ -0,0 +1,11 @@
+package coreAPIs;
+
+public class MathFunctions {
+
+    public static void main(String[] args) {
+        System.out.println(Math.random());
+
+        System.out.println((int) (Math.random()*6));
+
+    }
+}
