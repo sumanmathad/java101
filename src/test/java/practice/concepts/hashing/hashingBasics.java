fb8a53cde3dde49071de8a030f2792a93300642a
suman.mathad <suman.mathad@oracle.com>
2024-12-14 19:19:52 +0530

    java concepts updates

---
+++ src/test/java/practice/concepts/hashing/hashingBasics.java
@@ -0,0 +1,19 @@
+package practice.concepts.hashing;
+
+public class hashingBasics {
+
+    public static void main(String[] args) {
+
+        System.out.println('a');
+        System.out.println((int)'a');    //ascii
+
+
+
+
+
+
+    }
+
+
+
+}
